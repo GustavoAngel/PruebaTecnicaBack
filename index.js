74d5ad2568d4a01d0e2e59cbedce507c969151e8
GustavoAngel <gustavoandradeangel@hotmail.com>
2022-11-22 17:39:17 -0600

    su

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var cors = require('cors')
 const app= express();
 
 var corsOptions = {
-    origin: 'http://localhost:8080/',
+    origin: '',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
