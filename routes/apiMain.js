4da4825b24acea19334aaa498485a3bc752ecdba
GustavoAngel <gustavoandradeangel@hotmail.com>
2022-11-22 19:18:39 -0600

    documentation

--- routes/apiMain.js
+++ routes/apiMain.js
@@ -1,38 +1,42 @@
 const express= require('express');
 const employees = require('../models/employees');
-
 const router=express.Router();
 
 router.get('/',async function (req,res) {
-    //Se realiza la busqueda de todos los turnos de los empleados
-    const employeesList=await employees.find();
-    //Order item for date most recent
-    let data=employeesList.map(item=>new employees(item)).sort(function (a,b) {
-        if (a.date>b.date) {
-            return -1;    
-        }
-        else{
-            return 1;
-        }
-    });
-    
-    //Send result to client side
-    res.json(data);
+    try {
+         //Se realiza la busqueda de todos los turnos de los empleados
+        const employeesList=await employees.find();
+        //Order item for date most recent
+        let data=employeesList.map(item=>new employees(item)).sort(function (a,b) {
+            if (a.date>b.date) {
+                return -1;    
+            }
+            else{
+                return 1;
+            }
+        });
+        //Send result to client side
+        res.json(data);
+    } catch (error) {
+        res.json("Status",error);
+    }    
 });
 
 //Function for post request
 router.post('/',async (req,res) =>{
     //Variable for status of request
     let status='';
+    //Start try for this method
     try {
-        
+        //if body only contains 1 item                 
         if (req.body.length===1) {
-            
+            //parse object jason to object js
             let employee= new employees(req.body[0]);
-            
+            //Time cast utility
             const timeAux =req.body[0].PunchIn.split(':');
+            //Time cast utility
             const timeAuxOut =req.body[0].PunchOut.split(':');
-
+            //Replace value PunchIn
             employee.PunchIn = new Date(
                 employee.date.getFullYear(),
                 employee.date.getMonth(),
@@ -40,7 +44,7 @@ router.post('/',async (req,res) =>{
                 timeAux[0],    
                 timeAux[1],0,0
                 );
-
+            //Replace value PunchIn
             employee.PunchOut = new Date(
                 employee.date.getFullYear(),
                 employee.date.getMonth(),
@@ -48,7 +52,7 @@ router.post('/',async (req,res) =>{
                 timeAuxOut[0],    
                 timeAuxOut[1]
                 );
-                
+            //Save item                 
             await employee.save();   
         }
         else if (req.body.length>0) {
@@ -63,10 +67,10 @@ router.post('/',async (req,res) =>{
         }
        status= "Task saved";
     } catch (err) {
-        console.error(err);
         status= err;
     }
     finally{
+        //Result the request
         res.json({
             status:status
         });
@@ -74,47 +78,59 @@ router.post('/',async (req,res) =>{
 });
 
 router.put('/:id',async function (req,res) {
-    
-    let employee=new employees(req.body[0]);
-    const timeAux =req.body[0].PunchIn.split(':');
-    const timeAuxOut =req.body[0].PunchOut.split(':');
+    let status='';
+    try {
+        let employee=new employees(req.body[0]);
+        const timeAux =req.body[0].PunchIn.split(':');
+        const timeAuxOut =req.body[0].PunchOut.split(':');
 
-    employee.PunchIn = new Date(
-        employee.date.getFullYear(),
-        employee.date.getMonth(),
-        employee.date.getDate(),
-        timeAux[0],    
-        timeAux[1],0,0
-        );
+        employee.PunchIn = new Date(
+            employee.date.getFullYear(),
+            employee.date.getMonth(),
+            employee.date.getDate(),
+            timeAux[0],    
+            timeAux[1],0,0
+            );
 
-    employee.PunchOut = new Date(
-        employee.date.getFullYear(),
-        employee.date.getMonth(),
-        employee.date.getDate(),
-        timeAuxOut[0],    
-        timeAuxOut[1]
-        );
-
-    await employees.findByIdAndUpdate(req.params.id,employee);
-
-    res.json({
-        status:"Task updated"
-    });
-
+        employee.PunchOut = new Date(
+            employee.date.getFullYear(),
+            employee.date.getMonth(),
+            employee.date.getDate(),
+            timeAuxOut[0],    
+            timeAuxOut[1]
+            );
+        //Mongosse
+        await employees.findByIdAndUpdate(req.params.id,employee);
+        //Update message successful
+        status ='successful';
+    } catch (error) {
+        status=error;
+    }
+    finally{
+        //Message to client
+        res.json({
+            status:status
+        });
+    }
 });
 
 router.delete('/:id',async function (req,res) {
-    console.log(req.params.id);
+    //Variable for message
+    let status='';
     try {
-        await employees.findByIdAndRemove(req.params.id)       
+        //Delete item for de bd
+        await employees.findByIdAndRemove(req.params.id);       
+        //Update message successful
+        status ='successful';
     } catch (error) {
-        console.log(error);
+        status= error;
     }
-     
-
-    res.json({
-        status:"Task deleted"
-    });
+    finally{
+        //Message to client
+        res.json({
+            status:status
+        });
+    }
 });
 
 module.exports= router;
