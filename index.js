047f64261e7006dc8f2223bdbf5acddac5a161fd
GustavoAngel <gustavoandradeangel@hotmail.com>
2022-12-02 10:58:32 -0600

    1

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ var cors = require('cors')
 
 const app= express();
 
-var corsOptions = {
-    origin: '',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-
-  app.use(cors());
+// var corsOptions = {
+//     origin: '',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
+  app.use(cors({
+    origin: ['https://vermillion-rolypoly-82ef9b.netlify.app/', 'http://192.168.1.157:8080/']
+}));
+ // app.use(cors());
 //mongodb+srv://Kumamon001:xMwrPx241EdZ093p@cluster0.xowycrk.mongodb.net/?retryWrites=true&w=majority
   mongoose.connect('mongodb+srv://Kumamon001:xMwrPx241EdZ093p@cluster0.xowycrk.mongodb.net/?retryWrites=true&w=majority')
     .then(db=> console.log('DB is connected'))
