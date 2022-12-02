c3019d3d76403a92f55823bac2293f6152ca914f
GustavoAngel <gustavoandradeangel@hotmail.com>
2022-12-02 11:03:14 -0600

    121

--- index.js
+++ index.js
@@ -9,9 +9,29 @@ const app= express();
 //     origin: '',
 //     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 //   }
-  app.use(cors({
-    origin: ['https://vermillion-rolypoly-82ef9b.netlify.app/', 'http://192.168.1.157:8080/']
-}));
+//   app.use(cors({
+//     origin: ['https://vermillion-rolypoly-82ef9b.netlify.app/', 'http://192.168.1.157:8080/']
+// }));
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://vermillion-rolypoly-82ef9b.netlify.app/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
  // app.use(cors());
 //mongodb+srv://Kumamon001:xMwrPx241EdZ093p@cluster0.xowycrk.mongodb.net/?retryWrites=true&w=majority
   mongoose.connect('mongodb+srv://Kumamon001:xMwrPx241EdZ093p@cluster0.xowycrk.mongodb.net/?retryWrites=true&w=majority')
